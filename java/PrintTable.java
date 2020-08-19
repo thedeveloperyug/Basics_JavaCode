7f91de4fbf73ca591f629c121b86ceca26bf710d
Yogesh Pandey <incredibleyug@gmail.com>
2020-08-19 17:01:13 +0530

    How to Write Table in Java.

--- java/PrintTable.java
+++ java/PrintTable.java
@@ -2,20 +2,16 @@ import java.util.Scanner;
 
 public class PrintTable {
     public static void main(String[] args) {
-
-
-int i;
+        
+        int i;
 
         Scanner me = new Scanner(System.in);
         System.out.print("Enter any number to print Table = ");
         int a = me.nextInt();
-
         System.out.println("\n");
-
         for(i=1; i<=10; i++){
             System.out.println("\t"+a +" * "+ i + " = " + i*a);
         }
         System.out.println("\n");
     }
-
 }
